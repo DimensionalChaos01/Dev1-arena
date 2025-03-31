fb1ecb5b69515c37671d069f4ce16c4b6c5fd789
DimensionalChaos01 <146570379+DimensionalChaos01@users.noreply.github.com>
2025-03-30 23:32:52 -0700

    pain and misery

--- arena/Assets/scripts/gamebehavior.cs
+++ arena/Assets/scripts/gamebehavior.cs
@@ -1,11 +1,13 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 using UnityEngine.UI;
+using System.Collections.Generic;
+
 
 using UnityEngine.SceneManagement;
 using CustomExtensions;
+using TMPro;
+
 public class gamebehavior : MonoBehaviour, IManager
 {
     public bool showwinscreen = false;
